78b34454286899e17343d3f6896b494aead78183
RafaBusch <rbusch12433@gmail.com>
2023-01-06 10:15:17 -0300

    pasta-1

--- primeiro-script/main.tf
+++ primeiro-script/main.tf
@@ -1,18 +1,26 @@
+terraform {
+  required_version = "1.3.7"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.48.0"
+    }
+  }
+}
+
 provider "aws" {
-    region = "us-west-1"
+  region = "us-west-1"
 }
 
 resource "aws_s3_bucket" "my-test-bucket" {
-  bucket = "bucketcriadocomterraformbusch"
+  bucket = "bucketcriadocomterraformbusch-2"
 
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
     Managedby   = "terraform"
+    Owner       = "Rafael"
+    Atualizado  = "05/12"
   }
-}
-
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.b.id
-  acl    = "private"
 }
